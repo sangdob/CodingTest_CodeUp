372e5735b1222c08a7163f35a7a8e8c62f2dd241
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-01 09:06:17 +0900

    feat : Code Up 103, 106 코드 풀이

---
+++ src/main/java/org/example/upto200/Code103.java
@@ -0,0 +1,8 @@
+package org.example.upto200;
+
+public class Code103 {
+    public static void main(String[] args) {
+
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/org/example/upto200/Code106.java
@@ -0,0 +1,10 @@
+package org.example.upto200;
+
+public class Code106 {
+    public static void main(String[] args) {
+        int a = Integer.MIN_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        System.out.println(a + " " + b);
+    }
+}
