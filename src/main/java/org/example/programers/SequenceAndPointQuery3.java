6865560ba2a4a2947f7349636dbc7755532edd6e
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-02 09:40:20 +0900

    feat : Programmers 특별한 2차원 배열 풀이

--- src/main/java/org/example/programers/SequenceAndPointQuery3.java
+++ src/main/java/org/example/programers/SequenceAndPointQuery3.java
@@ -5,15 +5,12 @@ import java.util.Arrays;
 public class SequenceAndPointQuery3 {
     public static int[] solution(int[] arr, int[][] queries){
         int[] answer = Arrays.copyOf(arr, arr.length);
-
         int temp = 0;
-
         for (int i = 0; i < queries.length; i++) {
             temp = answer[queries[i][0]];
             answer[queries[i][0]] = answer[queries[i][1]];
             answer[queries[i][1]] = temp;
         }
-
         return answer;
     }
 
