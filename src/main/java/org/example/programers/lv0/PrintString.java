c2366afa9ac21f860c45508802fba460056878c7
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-06 22:45:19 +0900

    feat : LV0 문자열 출력하기

---
+++ src/main/java/org/example/programers/lv0/PrintString.java
@@ -0,0 +1,11 @@
+package org.example.programers.lv0;
+
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLine());
+    }
+}
