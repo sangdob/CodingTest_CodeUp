ab6da1e4ea5d3ed082e25bfc49768e8933952f8e
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-06 22:46:41 +0900

    feat : LV0 A와 B 출력하기

---
+++ src/main/java/org/example/programers/lv0/printAAndB.java
@@ -0,0 +1,14 @@
+package org.example.programers.lv0;
+
+import java.util.Scanner;
+
+public class printAAndB {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    }
+}
